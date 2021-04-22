fd152c70895826cb5f8b28abe938d0165ed60911
Rafael Gomes <rafaelgt@ciandt.com>
2021-04-22 10:39:42 -0300

    Average Exercise

---
+++ average/averageExercise.php
@@ -0,0 +1,13 @@
+<?php 
+    $numbers = array(10, 20, 30, 45);
+
+    function average($number) {
+        $total = 0;
+        foreach($number as $numbers) {
+            $total += $numbers;
+        }
+        $average = $total/count($number);
+        echo "Average: $average";
+    }
+    average($numbers);
+?>
---
+++ average/readme.md
@@ -0,0 +1,1 @@
+# Write a function that receive a undetermined number of parameters and returns the average
